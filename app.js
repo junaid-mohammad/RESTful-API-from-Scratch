c7a7992411880115adb364d1dd5e2b0d28aaaca4
Junaid <mohammad.junaidarif@mail.mcgill.ca>
2023-01-21 19:16:00 -0500

    Initial Commit.

---
+++ app.js
@@ -0,0 +1,130 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikidb",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+mongoose.set("strictQuery", true);
+
+const articleSchema = new mongoose.Schema ({
+ title: String,
+ content: String
+});
+
+const Article = mongoose.model("Article", articleSchema);
+
+
+//TODO
+
+////////////////////////////// REQUESTS TARGETING ALL ARTICLES ///////////////////////////////
+app.route("/articles")
+
+  .get(function(req, res) {
+    Article.find(function(err, foundArticles) {
+      if (!err) {
+        res.send(foundArticles);
+      } else {
+        res.send(err);
+      }
+    });
+  })
+
+  .post(function(req, res) {
+    const newArticle = new Article ({
+      title: req.body.title,
+      content: req.body.content
+    });
+    newArticle.save(function(err) {
+      if (!err) {
+        res.send("Successfully added a new article.");
+      } else {
+        res.send (err);
+      }
+    });
+  })
+
+  .delete(function(req, res) {
+    Article.deleteMany(function(err) {
+      if (!err) {
+        res.send ("Successfully deleted all the articles.");
+      } else {
+        res.send(err);
+      }
+    });
+  });
+
+////////////////////////////// REQUESTS TARGETING A SPECIFIC ARTICLE ///////////////////////////////
+
+app.route("/articles/:articleTitle")
+
+  .get(function(req, res) {
+    Article.findOne({title: req.params.articleTitle}, function(err, foundArticle) {
+      if (foundArticle) {
+        res.send(foundArticle);
+      } else {
+        res.send("No article matching that title was found.")
+      }
+    });
+  })
+
+  .put(function(req, res) {
+    Article.updateOne(
+      {title: req.params.articleTitle},
+      {title: req.body.title, content: req.body.content},
+      {overwrite: false},       // Angela used true. Does not work if I use true. Acts as a patch in my case.
+      function(err) {
+        if (!err) {
+          res.send("Successfully updated the article.");
+        } else {
+          res.send("Unable to update article.");
+        }
+      }
+    );
+  })
+
+  .patch(function(req, res) {
+    Article.updateOne(
+      {title: req.params.articleTitle},
+      {$set: req.body},
+      function(err) {
+        if (!err) {
+          res.send("Successfully updated the article.");
+        } else {
+          res.send(err);
+        }
+      }
+    );
+  })
+
+  .delete(function (req, res) {
+    Article.deleteOne(
+      {title: req.params.articleTitle},
+      function (err) {
+        if (!err) {
+          res.send("Delelted Corresponding Article Successfully");
+        } else {
+          res.send(err);
+        }
+      }
+    );
+  });
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
